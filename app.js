9518393d41b1b2abf1a9be602a53ac19005cb69e
NaserAlqimlas <naser.alqimlas@gmail.com>
2021-10-21 18:22:52 +0300

    still figuring stuff out

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+
+app.get("/home", (req, res) => {
+  res.status(200).json({ message: "hi" });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on port: ${process.env.PORT}`)
+);
